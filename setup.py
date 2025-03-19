bc8d89536dfe9ef1e9e3878bde8e7599fa2a17b4
Yash_Natholia <138648244+Cvssvay@users.noreply.github.com>
2025-03-19 21:08:17 +0530

    Files added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Reddit',
+    version= '0.0.0',
+    author= 'Yash',
+    author_email= 'yashnatholia123@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
